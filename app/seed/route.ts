9eca29ac23a3912e7e7107ba30b99ecedd17c572
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2025-04-30 22:41:55 -0600

    Seed Script Fix

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -3,35 +3,40 @@ import { sql } from "@/app/lib/utils";
 
 
 async function seedTickets() {
-    await sql`
-    CREATE TABLE IF NOT EXISTS tickets (
-      id SERIAL PRIMARY KEY,
-      number INTEGER NOT NULL,
-      status TEXT NOT NULL CHECK (status IN ('waiting', 'serving', 'served')),
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-    );
+
+  await sql`
+  DROP TABLE IF EXISTS tickets;
   `;
 
-    for (const ticket of tickets) {
-        await sql`
+  await sql`
+  CREATE TABLE IF NOT EXISTS tickets (
+    id SERIAL PRIMARY KEY,
+    number INTEGER UNIQUE NOT NULL, 
+    status TEXT NOT NULL CHECK (status IN ('waiting', 'serving', 'served')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`;
+
+  for (const ticket of tickets) {
+    await sql`
       INSERT INTO tickets (number, status)
       VALUES (${ticket.number}, ${ticket.status})
       ON CONFLICT (number) DO NOTHING;
     `;
-    }
+  }
 }
 
 
 export async function GET() {
-    try {
-        console.log("Seeding database...");
-        await seedTickets();
+  try {
+    console.log("Seeding database...");
+    await seedTickets();
 
-        return new Response("Database seeded successfully", { status: 200 });
+    return new Response("Database seeded successfully", { status: 200 });
 
-    } catch (error) {
-        console.error("Error seeding database:", error);
-        return new Response("Error seeding database", { status: 500 });
-    }
+  } catch (error) {
+    console.error("Error seeding database:", error);
+    return new Response("Error seeding database", { status: 500 });
+  }
 }
